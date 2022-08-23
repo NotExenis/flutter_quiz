a02a55921cf88bad2cec3fd5a1c514bec810fdea
Oude Luttikhuis, Robin - Student Rijn IJssel <00277188@rijnijssel.nl>
2022-08-23 13:30:08 +0200

    finised

--- quiz_screen.dart
+++ quiz_screen.dart
@@ -30,6 +30,7 @@ class _QuizScreenState extends State<QuizScreen> {
           ),
           _questionWidget(),
           _answerList(),
+          _nextButton(),
         ]),
       ),
     );
@@ -44,16 +45,103 @@ class _QuizScreenState extends State<QuizScreen> {
         ),
       ),
       const SizedBox(height: 20),
-      Text(
-        questionlist[currentQuestionIndex].questionText,
+      Text(questionlist[currentQuestionIndex].questionText,
           style: const TextStyle(
             color: Colors.white,
           ))
     ]);
   }
 
-  _answerList(){
+  _answerList() {
     return Column(
-      children: questionlist[currentQuestionIndex];
+      children: questionlist[currentQuestionIndex]
+          .answerList
+          .map(
+            (e) => _answerButton(e),
+          )
+          .toList(),
+    );
+  }
 
+  Widget _answerButton(Answer answer) {
+    bool isSelected = answer == selectedAnswer;
+
+    return Container(
+      width: double.infinity,
+      margin: const EdgeInsets.symmetric(vertical: 8),
+      height: 48,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+            shape: const StadiumBorder(),
+            primary: isSelected ? Colors.blue : Colors.white,
+            onPrimary: isSelected ? Colors.white : Colors.black),
+        onPressed: () {
+          if (selectedAnswer == null) {
+            if (answer.isCorrect) {
+              score++;
+            }
+
+            setState(
+              () {
+                selectedAnswer = answer;
+              },
+            );
+          }
+        },
+        child: Text(answer.answerText),
+      ),
+    );
+  }
+
+  _nextButton() {
+    bool isLastQuestion = false;
+    if (currentQuestionIndex == questionlist.length - 1) {
+      isLastQuestion = true;
+    }
+
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.5,
+      height: 48,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+            shape: const StadiumBorder(),
+            primary: Colors.redAccent,
+            onPrimary: Colors.white),
+        onPressed: () {
+          if (isLastQuestion) {
+            showDialog(context: context, builder: (_) => _showScoreDialog());
+          } else {
+            setState(() {
+              selectedAnswer = null;
+              currentQuestionIndex++;
+            });
+          }
+        },
+        child: Text(isLastQuestion ? "submit" : "Next"),
+      ),
+    );
+  }
+
+  _showScoreDialog() {
+    bool isPassed = false;
+    if (score >= questionlist.length * 0.6) {
+      isPassed = true;
+    }
+    String title = isPassed ? "Passed" : "Failed";
+    return AlertDialog(
+      title: Text(
+        "$title | Score is $score",
+      ),
+      content: ElevatedButton(
+          child: const Text('Restart'),
+          onPressed: () {
+            setState(() {
+              Navigator.pop(context);
+              currentQuestionIndex = 0;
+              score = 0;
+              selectedAnswer = null;
+            });
+          }),
+    );
+  }
 }
